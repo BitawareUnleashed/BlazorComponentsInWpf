96e38d8a0de9cfa99bffa7b7434629500b6222fa
BitawareUnleashed <dario.benevento@gmail.com>
2022-12-22 00:01:37 +0100

    First commit

---
+++ WpfMudBlazor/MainLayout.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMudBlazor;
+
+public partial class MainLayout
+{
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+    }
+}
---
+++ WpfMudBlazor/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+
+
+namespace WpfMudBlazor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddMudServices();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+            
+
+
+        }
+    }
+}
