dcf6f5d75b7b01be1802ab7b594ed5e29007c985
BitawareUnleashed <dario.benevento@gmail.com>
2023-04-17 23:12:56 +0200

    Deleted commented and useless code

--- WpfMudBlazor/Models/TextChanged.cs
+++ WpfMudBlazor/Models/TextChanged.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WpfMudBlazor.Models;
+﻿namespace WpfMudBlazor.Models;
 
 public record TextChanged(string Text);
 
